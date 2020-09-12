d287f090e337dc73a749853a36c3aafe3691925d
hunny345686 <hunny345686@gmail,com>
2020-09-12 17:24:10 +0530

    Updates

---
+++ precache-manifest.4bcaaf3f92a9e499dfa3dcde84372076.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660b4411af087c266522b11dfc6e40ba",
+    "url": "/reactCoronaWebsite/index.html"
+  },
+  {
+    "revision": "3666ef6383729f3d1c01",
+    "url": "/reactCoronaWebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "2dc4604eeb2fd584a874",
+    "url": "/reactCoronaWebsite/static/css/main.94df9088.chunk.css"
+  },
+  {
+    "revision": "3666ef6383729f3d1c01",
+    "url": "/reactCoronaWebsite/static/js/2.c376fb89.chunk.js"
+  },
+  {
+    "revision": "9f77ee135f38b2e06663b7556802c959",
+    "url": "/reactCoronaWebsite/static/js/2.c376fb89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc4604eeb2fd584a874",
+    "url": "/reactCoronaWebsite/static/js/main.40e8391a.chunk.js"
+  },
+  {
+    "revision": "9ebf12c553cfcd0b340e",
+    "url": "/reactCoronaWebsite/static/js/runtime-main.6c0d6224.js"
+  },
+  {
+    "revision": "2fe2b720299ed66fd99e10bf827445a3",
+    "url": "/reactCoronaWebsite/static/media/lep1.2fe2b720.jpg"
+  },
+  {
+    "revision": "06713574266a9a11cdbe21af09c6aa91",
+    "url": "/reactCoronaWebsite/static/media/lep2.06713574.jpg"
+  },
+  {
+    "revision": "9468a22c20f9471b157afc598bc4ca91",
+    "url": "/reactCoronaWebsite/static/media/stulep.9468a22c.jpg"
+  }
+]);
